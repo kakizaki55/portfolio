99019d19da7c47ce9c04467a6a72c2aa170ac589
minoka kakizaki <minoka.kakizaki@gmail.com>
2023-08-15 16:32:37 -0700

    have a pretty cool pixel render

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import './App.css'
 import Main from './R3f'
 import { Canvas } from "@react-three/fiber"
+import * as THREE from 'three'
 
 function App() {
   // const [count, setCount] = useState(0)
 
+  const cameraSettings = {
+    fov: 75,
+    // zoom: 75,
+    near: 0.1,
+    far: 200,
+    position: [ 2, 2, 6 ]
+}
+
+
   return (
     <>
-      <Canvas>
+      <Canvas
+        // orthographic
+        // flat
+        // linear
+        dpr={ .25 }
+        gl={{
+          antialias: false,
+          toneMapping: THREE.ACESFilmicToneMapping,
+          // outputColorSpace: THREE.LinearSRGBColorSpace
+        }}
+        camera={cameraSettings}  >
         <Main/>
       </Canvas>
     </>
--- src/R3f/index.jsx
+++ src/R3f/index.jsx
@@ -1,6 +1,7 @@
 import React, { useRef }from 'react'
 import CameraController from "./CameraController/CameraController";
 import { useFrame } from "@react-three/fiber";
+import CustomObject from "./CustomObject/CustomObject";
 
 
 
@@ -11,9 +12,13 @@ const Main = () =>  {
   const groupRef = useRef()
 
   useFrame((state, delta) => {
-    console.log( delta)
     cubeRef.current.rotation.x += delta
     groupRef.current.rotation.y += delta
+
+    // const angle = state.clock.elapsedTime
+    // state.camera.position.x = Math.sin(angle + 2)* 6
+    // state.camera.position.z = Math.cos(angle)* 6
+    // state.camera.lookAt(0, 0, 0)
   })
 
   return (
@@ -21,6 +26,7 @@ const Main = () =>  {
       <CameraController/>
       <directionalLight position={[1,1,1]} />
       <ambientLight intensity={ 0.1 } />
+      <CustomObject/>
       <group ref={ groupRef }>
           <mesh position-x={ - 2 }>
               <sphereGeometry />
